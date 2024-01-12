c84f211cfbfbcb94fc3f104ec61c1c592f21c88e
yenvietsoft <128137927+binhkid2@users.noreply.github.com>
2024-01-12 03:12:30 +0000

    okk

---
+++ src/lib/Lang.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { availableLanguageTags, languageTag } from "../paraglide/runtime"
+import { translatePath } from "$lib/i18n"
+import { goto } from "$app/navigation"
+
+</script>
+
+
+<select on:change={(e) => goto(translatePath("/", e.target?.value))}>
+	{#each availableLanguageTags as lang}
+		<option value={lang} selected={lang === languageTag()}>{lang}</option>
+	{/each}
+</select>
